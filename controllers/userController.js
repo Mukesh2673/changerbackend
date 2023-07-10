fbf8efdeb2229ab585ae980c5afb08be9fb99553
AgweBryan <agwebryanmunakwa@gmail.com>
2023-07-10 18:02:02 +0100

    Created follow user api

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,47 +1,84 @@
-const { User } = require('../models');
+const { User } = require("../models");
 
 exports.getUser = async (req, res, next) => {
-    try {
-        const user = await User.findById(req.params.id);
-        return res.json(user);
-    } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
-}
+  try {
+    const user = await User.findById(req.params.id);
+    return res.json(user);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 exports.getUserByUID = async (req, res, next) => {
-    try {
-        const user = await User.findOne({uid: req.params.uid});
-        return res.json(user);
-    } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
-}
+  try {
+    const user = await User.findOne({ uid: req.params.uid });
+    return res.json(user);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
 exports.createUser = async (req, res, next) => {
-    try {
-        const existingUser = await User.findOne({username: req.body.username});
-        if (existingUser) {
-            return res.status(403).json({message: "username-exists"})
-        }
-    } catch (error) {
-        return res.status(500).json({message: error.message})
+  try {
+    const existingUser = await User.findOne({ username: req.body.username });
+    if (existingUser) {
+      return res.status(403).json({ message: "username-exists" });
     }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
 
-    const user = new User({
-        username: req.body.username,
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        dob: req.body.dob,
-        uid: req.body.uid,
-        email: req.body.email,
-    })
+  const user = new User({
+    username: req.body.username,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    dob: req.body.dob,
+    uid: req.body.uid,
+    email: req.body.email,
+  });
 
-    try {
-        const savedUser = await user.save();
-        return res.status(200).json(savedUser);
-    } catch (error) {
-        return res.status(500).json({message: error.message})
-    }
+  try {
+    const savedUser = await user.save();
+    return res.status(200).json(savedUser);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
 
-}
+exports.followUser = async (req, res) => {
+  const { cuid, fuid } = req.params;
+
+  try {
+    // add current user's id to users followers array
+    await User.updateOne({ _id: fuid }, { $push: { followers: cuid } });
+
+    // Add users id to my followings array
+    await User.updateOne({ _id: cuid }, { $push: { following: fuid } });
+
+    // Get followers
+    const user = await User.findById({ _id: fuid }).select("followers");
+
+    return res.status(200).json({ user });
+  } catch (e) {
+    return res.status(404).json({ error: e.message });
+  }
+};
+
+exports.unFollowUser = async (req, res) => {
+  const { cuid, fuid } = req.params;
+
+  try {
+    // remove current user's id from users followers array
+    await User.updateOne({ _id: fuid }, { $pull: { followers: cuid } });
+
+    // remove users id from my followings array
+    await User.updateOne({ _id: cuid }, { $pull: { following: fuid } });
+
+    // Get followers
+    const user = await User.findById({ _id: fuid }).select("followers");
+
+    return res.status(200).json({ user });
+  } catch (e) {
+    return res.status(404).json({ error: e.message });
+  }
+};
